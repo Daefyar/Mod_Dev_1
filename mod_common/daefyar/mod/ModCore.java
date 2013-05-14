ab2c119578ae9ae39fb83a9337eaea2dfaa04ddc
Daefyer <Daefyer@gmail.com>
2013-05-14 14:58:02 -0700

    First files

---
+++ mod_common/daefyar/mod/ModCore.java
@@ -0,0 +1,33 @@
+package daefyar.mod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import daefyar.mod.lib.Reference;
+
+@Mod (modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class ModCore
+{
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        
+    }
+    
+    @Init
+    public void Init(FMLInitializationEvent event)
+    {
+        
+    }
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        
+    }
+
+
+}
---
+++ mod_common/daefyar/mod/lib/Reference.java
@@ -0,0 +1,8 @@
+package daefyar.mod.lib;
+
+public class Reference
+{
+public static final String MOD_ID = "Mod";
+public static final String NAME = "Mod";
+public static final String VERSION = "0.0.1";
+}
